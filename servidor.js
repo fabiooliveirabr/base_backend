1654f1372349fe2adaaf7ff4979f948aed5bf2d9
Fábio Oliveira <fabiodeoliveirabr@gmail.com>
2025-06-26 20:00:55 -0300

    base ok

---
+++ servidor.js
@@ -0,0 +1,12 @@
+const db = require('./conexao');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor rodando na porta 3000');
+});
