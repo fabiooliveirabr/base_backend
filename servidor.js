7867ea348df0b3185720bf1f0fcc0d50e9e9dff5
Fábio Oliveira <fabiodeoliveirabr@gmail.com>
2025-06-27 19:44:01 -0300

    aula 27/06

--- servidor.js
+++ servidor.js
@@ -39,6 +39,16 @@ app.get('/clientes', (req, res)=>{
                 return res.json(resultado)
              })
 })
+
+app.get('/clientes/:id', (req, res)=>{
+    const id = req.params.id
+    db.query('SELECT * FROM tb_clientes WHERE id_cliente=?', [id],
+              (erro, resultado)=>{
+                if(erro){return res.json({msg:"Falha ao consultar!"+erro.message})}
+                return res.json(resultado)
+             })
+})
+
 app.post('/clientes', (req, res)=>{
     const {nome, email} = req.body
     db.query('INSERT INTO tb_clientes (nome, email) VALUES (?, ?)',
@@ -59,6 +69,53 @@ app.post('/chamados', (req, res)=>{
                 return res.json({msg:"Cadastrado com sucesso!"})
             })
 })
+
+app.delete('/chamado/:id_chamado', (req, res)=>{
+    const id_chamado = req.params.id_chamado
+    db.query('DELETE FROM tb_chamados WHERE id_chamado=?',
+              [id_chamado], (erro, resultado)=>{
+            if(erro){return res.json({msg:"Falha ao deletar!"+erro.message})}
+            if(resultado.length == 0){return res.json({msg:"Nada alterado"})}
+            return res.json({msg:"Deletado com sucesso!"})
+    })
+})
+app.delete('/cliente/:id_cliente', (req, res)=>{
+    const id_cliente = req.params.id_cliente
+    db.query('DELETE FROM tb_clientes WHERE id_cliente=?',
+              [id_cliente], (erro, resultado)=>{
+            if(erro){return res.json({msg:"Falha ao deletar!"+erro.message})}
+            if(resultado.length == 0){return res.json({msg:"Nada alterado"})}
+            return res.json({msg:"Deletado com sucesso!"})
+    })
+})
+app.delete('/chamado/:id_chamado', (req, res)=>{
+    const id_chamado = req.params.id_chamado
+    db.query('DELETE FROM tb_chamados WHERE id_chamado=?',
+              [id_chamado], (erro, resultado)=>{
+            if(erro){return res.json({msg:"Falha ao deletar!"+erro.message})}
+            if(resultado.length == 0){return res.json({msg:"Nada alterado"})}
+            return res.json({msg:"Deletado com sucesso!"})
+    })
+})
+
+app.put('/chamado',(req,res)=>{
+      const {assunto, descricao, fk_id_cliente, prioridade, id_chamado}
+      = req.body
+      db.query(`UPDATE tb_chamados SET assunto=?, descricao=?,
+              fk_id_cliente=?, prioridade=? WHERE id_chamado=?`,
+              [assunto, descricao, fk_id_cliente, prioridade, id_chamado],
+              (erro, resultado)=>{
+                   if(erro){return res.json({msg:"Falha ao atualizar!"+erro.message})}
+                   return res.json({msg:"Atualizado com sucesso!"})
+              })
+})
+
+
+
+
+
+
+
 
 app.listen(3000, ()=>{
     console.log('Servidor rodando na porta 3000');
